55fe0efdc5b90e68d03151d78f9acc413e9bd17e
mohammad khedmati <mohammad.khedmati2012@gmail.com>
2025-12-15 13:28:43 +0330

    Update title and branding references from 'Frigate' to 'VisionGuard'

--- web/src/components/overlay/LogInfoDialog.tsx
+++ web/src/components/overlay/LogInfoDialog.tsx
@@ -118,7 +118,7 @@ function useHelpfulLinks(content: string | undefined) {
         link: getLocaleDocUrl(
           "configuration/record#will-frigate-delete-old-recordings-if-my-storage-runs-out",
         ),
-        text: "Frigate Automatic Storage Cleanup",
+        text: "VisionGuard Automatic Storage Cleanup",
       });
     }
 
